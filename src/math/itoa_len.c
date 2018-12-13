1cd5de9822c350b70fa2bce3c0368d43c4a85947
jacksonwb <jbeall@e1z3r3p3.42.us.org>
2018-12-12 19:30:37 -0800

    src/math/itoa_len.c

--- src/math/itoa_len.c
+++ src/math/itoa_len.c
@@ -6,11 +6,11 @@
 /*   By: jbeall <jbeall@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 14:24:47 by jbeall            #+#    #+#             */
-/*   Updated: 2018/12/12 14:31:31 by jbeall           ###   ########.fr       */
+/*   Updated: 2018/12/12 19:30:23 by jbeall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int itoa_len(int n)
+unsigned int	itoa_len(int n)
 {
 	unsigned int count;
 
